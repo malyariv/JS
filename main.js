575dced050e600694420f8ba5ca5cd247eb4ec9c
= <=>
2019-05-28 22:39:24 +0400

    next

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
-/*'use strict';
-do
-	var userName = prompt("Как твое имя, амиго?");
-while(!userName);
-//document.write('<h1 type="center">Олла, '+ userName +'!</h1>');
-//document.write("Я загадал число от 0 до 100. Рискнешь угадать?");
-alert("Я загадал число от 0 до 100. Рискнешь угадать?");
-var num = Math.round(100*Math.random());
-console.log(num);
-var counter = 0;
-var userNumber=-1;
-while (userNumber != num) {
-	userNumber = prompt("Действуй!");
-	counter++;
-	userNumber>num? alert("Больше"): userNumber===num? alert("Угадал"): alert("меньше");
-}
-if (counter % 10 <5) document.write("Вам понадобилось "+counter+" попытки");
-else document.write("Вам понадобилось "+counter+" попыток");
-*/
-console.log(""+null);
+"use strict";
+
+let box = document.getElementById("box"),
+	btn = document.getElementsByTagName("button"),
+	circle = document.getElementsByClassName("circle"),
+	heart = document.querySelectorAll(".heart"),
+	singleHeart = document.querySelector(".heart");
